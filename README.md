60053be05ec76bf2e7111ed4cf75183423c6ddbb
RobLanf <rob.lanfear@gmail.com>
2014-12-08 20:32:09 +1100

    added MIT licenses
    to all files

--- README.md
+++ README.md
@@ -145,3 +145,28 @@ In brief, the script:
 1. Changes "PLoS ONE" in journal.name to "PLoS One", because both names are used in the database, but refer to a single journal
 
 2. Sets all variables to appropriate types for R. E.g. changes the text 'True' to the logical variable 'TRUE' recognised by R.
+
+License
+-------
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Robert Lanfear
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- data_cleaning.r
+++ data_cleaning.r
@@ -1,5 +1,28 @@
 # This script process the data for use in R. Many of the steps below are unnecessary, but applying all of them will ensure that the resulting csv file is properly processed for easy and efficient use in R.
 
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Robert Lanfear
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 ###############################################################################
 # Set these variables before you run the script:
 
@@ -39,7 +62,7 @@ d$type.results <- as.factor(as.character(d$type.results))
 d$type.methods <- as.factor(as.character(d$type.methods))
 d$folder.name <- as.factor(as.character(d$folder.name))
 
-# Pre-process booleans 
+# Pre-process booleans
 d$abstract.found <- as.logical(as.character(d$abstract.found))
 d$abstract.experiment <- as.logical(as.character(d$abstract.experiment))
 
